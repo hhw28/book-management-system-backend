cd8cd6a3e3f9fca5cf6834ab292916bc94eb35bd
wanghuan <196912334@qq.com>
2024-10-31 11:05:03 +0800

    使用winston打印日志

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,89 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import * as path from 'path';
+import { storage } from './my-file-storage';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import * as fs from 'fs';
+import { MyLogger } from './Mylogger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  private readonly logger = new MyLogger();
+
   @Get()
   getHello(): string {
+    this.logger.log('getHello', AppController.name);
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FilesInterceptor('files', 20, {
+      dest: 'uploads',
+    }),
+  )
+  uploadFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+
+    const fileName = body.name.match(/(.+)\-\d+$/)[1];
+    const chunkDir = 'uploads/chunks_' + fileName;
+
+    if (!fs.existsSync(chunkDir)) {
+      fs.mkdirSync(chunkDir);
+    }
+    fs.cpSync(files[0].path, chunkDir + '/' + body.name);
+    fs.rmSync(files[0].path);
+  }
+
+  @Get('merge')
+  merge(@Query('name') name: string) {
+    const chunkDir = 'uploads/chunks_' + name;
+
+    const files = fs.readdirSync(chunkDir);
+
+    let startPos = 0;
+    let count = 0;
+
+    files.map((file) => {
+      const filePath = chunkDir + '/' + file;
+      const stream = fs.createReadStream(filePath);
+      stream
+        .pipe(
+          fs.createWriteStream('uploads/' + name, {
+            start: startPos,
+          }),
+        )
+        .on('finish', () => {
+          count++;
+
+          if (count === files.length) {
+            fs.rm(
+              chunkDir,
+              {
+                recursive: true,
+              },
+              () => {},
+            );
+          }
+        });
+
+      startPos += fs.statSync(filePath).size;
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { MyLogger } from './Mylogger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -10,6 +11,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   app.useStaticAssets(join(__dirname, '../uploads'), {prefix: '/uploads'});
+
+  app.enableCors();
+
+  app.useLogger(new MyLogger());
 
   await app.listen(process.env.PORT ?? 3000);
 }
